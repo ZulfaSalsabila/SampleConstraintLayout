1436c7b21e59c4030c4fb2d61228d5f048af201b
ZulfaSalsabila <zulfa130802@gmail.com>
2022-03-06 09:36:41 +0700

    update

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Deklarasi variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada Layout
+        btnLogin = findViewById(R.id.btSignin);
+
+        //Menghubungkan variabel edemail dengan componen button pada Layout
+        edemail = findViewById(R.id.edEmail);
+
+        //Menghubungkan variabel edpassword dengan componen button pada Layout
+        edpassword = findViewById(R.id.edPassword);
+
+        //Membuat fungsi onclik pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //menyimpan input user di edittext email kedalam variabel nama
+                nama = edemail.getText().toString();
+
+                //menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+
+                if (nama.equals("zulfaaa.sls@gmail.com") && password.equals("zulfa138")) {
+                    //jika login berhasil
+                    Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_LONG).show();
+
+                } else if (password.equals("zulfa138")) {
+                    //jika Email salah
+                    Toast.makeText(getApplicationContext(), "Email yang anda masukkan salah", Toast.LENGTH_LONG).show();
+
+                } else if (nama.equals("zulfaaa.sls@gmail.com")) {
+                    //jika Password salah
+                    Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_LONG).show();
+
+                } else {
+                    //jika email dan password salah
+                    Toast.makeText(getApplicationContext(), "Email dan Password Salah", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
