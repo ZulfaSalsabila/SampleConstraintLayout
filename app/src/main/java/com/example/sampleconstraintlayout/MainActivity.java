a491b67203c2818a61621570b0c63274b4f40881
ZulfaSalsabila <zulfa130802@gmail.com>
2022-03-07 19:59:25 +0700

    update Activity_kedua dengan dua activity dan passing parameter

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -44,16 +44,31 @@ public class MainActivity extends AppCompatActivity {
                 //menyimpan input user di edittext password kedalam variabel password
                 password = edpassword.getText().toString();
 
+                //mengeset email yang benar
+                String email = "zulfaaa.sls@gmail.com";
 
-                if (nama.equals("zulfaaa.sls@gmail.com") && password.equals("zulfa138")) {
+                //mengeset password yang benar
+                String pass = "zulfa138";
+
+
+                if (nama.equals(email) && password.equals(pass)) {
                     //jika login berhasil
                     Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_LONG).show();
-
-                } else if (password.equals("zulfa138")) {
+                    Bundle b = new Bundle();
+                    b.putString("a",nama.trim());
+                    b.putString("b",password.trim());
+                    Intent i = new Intent(getApplicationContext(),Tugas2.class);
+                    i.putExtras(b);
+                    startActivity(i);
+                }
+                else if (nama.isEmpty()||password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Email dan Password tidak boleh kosong",Toast.LENGTH_LONG).show();
+                }
+                else if (password.equals(pass)) {
                     //jika Email salah
                     Toast.makeText(getApplicationContext(), "Email yang anda masukkan salah", Toast.LENGTH_LONG).show();
-
-                } else if (nama.equals("zulfaaa.sls@gmail.com")) {
+                }
+                else if (nama.equals(email)) {
                     //jika Password salah
                     Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_LONG).show();
 
